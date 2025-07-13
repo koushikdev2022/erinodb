5c79614c9e680f12f43840d30e13893e553f0cbb
Abir Devx <abir.devx@gmail.com>
2025-07-13 11:40:21 +0530

    add admin migration

--- src/migrations/20250712120814-create_admin_table.js
+++ src/migrations/20250712120814-create_admin_table.js
@@ -6,72 +6,38 @@ module.exports = {
 
         await queryInterface.createTable('admins', {
             id: {
-                type: Sequelize.BIGINT,
-                autoIncrement: true,
-                allowNull: false,
-                primaryKey: true,
-            },
-            username: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true,
-            },
-            full_name: {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-            password: {
-                type: Sequelize.STRING,
-                allowNull: false,
-            },
-            email: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true,
-            },
-            mobile: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true,
-            },
-            otp: {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-            otp_expired_at: {
-                type: Sequelize.DATE,
-                allowNull: true,
-            },
-            role_id: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'roles',
-                    key: 'id',
-                },
-                onUpdate: 'CASCADE',
-                onDelete: 'CASCADE',
-                comment: "role id",
-            },
-            avatar: {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-            status: {
+                type: Sequelize.BIGINT, autoIncrement: true, allowNull: false, primaryKey: true,
+            }, username: {
+                type: Sequelize.STRING, allowNull: false, unique: true,
+            }, full_name: {
+                type: Sequelize.STRING, allowNull: true,
+            }, password: {
+                type: Sequelize.STRING, allowNull: false,
+            }, email: {
+                type: Sequelize.STRING, allowNull: false, unique: true,
+            }, mobile: {
+                type: Sequelize.STRING, allowNull: false, unique: true,
+            }, otp: {
+                type: Sequelize.STRING, allowNull: true,
+            }, validate_otp: {
+                type: Sequelize.DATE, allowNull: true, comment: "otp validate date"
+            }, otp_expired_at: {
+                type: Sequelize.DATE, allowNull: true, comment: "otp expired date"
+            }, role_id: {
+                type: Sequelize.INTEGER, allowNull: false, references: {
+                    model: 'roles', key: 'id',
+                }, onUpdate: 'CASCADE', onDelete: 'CASCADE', comment: "role id",
+            }, avatar: {
+                type: Sequelize.STRING, allowNull: true,
+            }, status: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
                 defaultValue: 1,
                 comment: "1 for => active 0 for => not active",
-            },
-            created_at: {
-                type: Sequelize.DATE,
-                allowNull: true,
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-            },
-            updated_at: {
-                type: Sequelize.DATE,
-                allowNull: true,
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            }, created_at: {
+                type: Sequelize.DATE, allowNull: true, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            }, updated_at: {
+                type: Sequelize.DATE, allowNull: true, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
             },
         });
     },
