9b4353d15ca123f9736e5ba39de3c05c648f4618
User <user@Users-MacBook-Air.local>
2025-07-26 14:56:30 +0530

    d

---
+++ src/migrations/20250726085521-change_columns_to_vendor_shops_table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.changeColumn('vendor_shops', 'lat', {
+      type: Sequelize.DOUBLE(10, 2),
+      allowNull: false,
+    });
+    await queryInterface.changeColumn('vendor_shops', 'long', {
+      type: Sequelize.DOUBLE(10, 2),
+      allowNull: false,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
