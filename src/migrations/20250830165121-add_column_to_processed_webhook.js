5f331733e5acb938160f2c98594cc77aa82e0fb4
Abir Devx <abir.devx@gmail.com>
2025-08-30 22:23:39 +0530

    new

--- src/migrations/20250830165121-add_column_to_processed_webhook.js
+++ src/migrations/20250830165121-add_column_to_processed_webhook.js
@@ -4,11 +4,6 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await Promise.all([
-      queryInterface.addColumn('processed_webhook_events', 'processed_at', {
-        type: Sequelize.DATE,
-        allowNull: true,
-        comment: 'The timestamp when the event was processed'
-      }),
       queryInterface.addColumn('processed_webhook_events', 'event_name', {
         type: Sequelize.STRING(255),
         allowNull: true,
@@ -19,7 +14,6 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     await Promise.all([
-      queryInterface.removeColumn('processed_webhook_events', 'processed_at'),
       queryInterface.removeColumn('processed_webhook_events', 'event_name')
     ]);
   }
