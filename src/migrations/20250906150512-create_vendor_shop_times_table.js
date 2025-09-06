08d20de2d883cca3df45e8a7d1e17b4ccae6d73c
soumyodip <soumyodip93@gmail.com>
2025-09-06 20:45:00 +0530

    d

---
+++ src/migrations/20250906150512-create_vendor_shop_times_table.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('vendor_shop_times', {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      vendor_shop_id: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+          comment: 'Reference to vendor shops table',
+      },
+      day_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        comment: 'Reference to vendor shops table',
+      },
+      start_time: {
+        type: Sequelize.TIME,
+        allowNull: true,
+      },
+      end_time: {
+        type: Sequelize.TIME,
+        allowNull: true,
+      },
+      shift_details: {
+        type: Sequelize.TINYINT,
+        allowNull: true,
+        defaultValue:1,
+        comment: '1 for 1st start, 2 for 2nd shift'
+      },
+      status: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 1,
+        comment: "0 = inactive, 1 = active"
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
