fdb0656a91034d984465e5628796a5df3b01d2de
Abir Devx <abir.devx@gmail.com>
2025-11-02 14:56:29 +0530

    new

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_CLIENT = process.env.DB_CLIENT;
 const DB_HOST = process.env.DB_HOST;
 
+console.log("DB_USERNAME:", DB_USERNAME, "DB_PASSWORD:", DB_PASSWORD, "DB_DATABASE:", DB_DATABASE, "DB_CLIENT:", DB_CLIENT, "DB_HOST:", DB_HOST);
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
     dialect: DB_CLIENT,
