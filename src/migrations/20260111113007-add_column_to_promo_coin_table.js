fb074e3354b9ed3f071711fc6396dd76e4156bf7
Abir Devx <abir.devx@gmail.com>
2026-01-11 17:10:47 +0530

    add

---
+++ src/migrations/20260111113007-add_column_to_promo_coin_table.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('promo_coins', 'transaction_type', {
+      type: Sequelize.ENUM('credit', 'debit'),
+      allowNull: true,
+      after: 'total_coin',
+      
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
