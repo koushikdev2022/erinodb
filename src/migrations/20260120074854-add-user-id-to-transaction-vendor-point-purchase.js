a109079b829e2df377d191d61362e096b79790af
soumyodip <soumyodip93@gmail.com>
2026-01-20 15:11:56 +0530

    d

---
+++ src/migrations/20260120074854-add-user-id-to-transaction-vendor-point-purchase.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('transaction_vendor_point_purchase', 'user_id', {
+      type: Sequelize.BIGINT,
+      allowNull: true,
+      
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
