efa24ad84a70c81b5ac903a08c93d81efc660c39
soumyodip <soumyodip93@gmail.com>
2026-01-23 23:40:45 +0530

    d

---
+++ src/migrations/20260123174555-add-columns-to-user-referrals--table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'user_referrals',
+      'expired_at',
+      {
+        type: Sequelize.DATE,
+        allowNull: false,
+        after: 'refered_user_id'
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
