562be17c308453b364517ba5ee79b5891136f589
soumyodip <soumyodip93@gmail.com>
2026-03-03 14:08:05 +0530

    d

---
+++ src/migrations/20260303081951-add_column_to_vendors_table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'vendors',
+      'merchant_id',
+      {
+        type: Sequelize.STRING,
+        allowNull: true,
+        after: 'last_name',
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
