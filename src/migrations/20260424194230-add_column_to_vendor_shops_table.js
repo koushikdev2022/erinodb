764eda7801fe3e684fb7599a7dc0263817a97fe2
soumyodip <soumyodip93@gmail.com>
2026-04-25 01:25:20 +0530

    d

---
+++ src/migrations/20260424194230-add_column_to_vendor_shops_table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'vendor_shops',
+      'shop_details',
+      {
+        type: Sequelize.TEXT('long'),
+        allowNull: true,
+        after: 'shop_address',
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
