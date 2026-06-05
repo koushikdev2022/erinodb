e2b92024198315fb4ab92df379714c2ac7cd8364
soumyodip <soumyodip93@gmail.com>
2026-06-06 03:19:36 +0530

    d

---
+++ src/migrations/20260601151910-create-admin-bank-details.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('admin_bank_account_details', {
+        id: {
+          type: Sequelize.BIGINT,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        account_number:{
+          type:Sequelize.STRING,
+          allowNull:false,
+        },
+        ifsc_code:{
+          type:Sequelize.STRING,
+          allowNull:false,
+        },
+        is_primary:{
+          type:Sequelize.TINYINT,
+          allowNull:false,
+          defaultValue:0,
+          comment: '1 = PRIMARY 0 = NOT PRIMARY',
+        },
+        status:{
+          type:Sequelize.TINYINT,
+          allowNull:false,
+          defaultValue:1,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        }
+      })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ src/migrations/20260605122350-create-excel-by-vendor.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+      await queryInterface.createTable('excel_by_vendors', {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      customer_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+
+      vendor_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+
+      vendor_shop_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+
+      json_data: {
+        type: Sequelize.JSON,
+        allowNull: true,
+      },
+
+      status: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 1,
+        comment: '1 = Active, 0 = Inactive',
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
